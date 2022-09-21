0abe64e3f274d4ef07bc027d78d5b3b6681903e5
BlackH3art <jaroslaw.musielak@outlook.com>
2022-09-21 20:17:16 +0200

    add: db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: ["dist/**/*.entity{.ts,.js}"],
+        bigNumberStrings: false,
+        logging: true,
+        synchronize: true,
+      })
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
